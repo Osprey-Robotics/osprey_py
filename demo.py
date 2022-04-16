c218d12f8b4968c36dfcd8cd86349bc875bdd39b
Nathan Nye <noproto@users.noreply.github.com>
2022-04-16 02:35:47 -0400

    Clean up demo.py a bit

--- demo.py
+++ demo.py
@@ -9,20 +9,29 @@ import curses
 
 all_motors = []
 
+# Constants
+SER_FRONT_LEFT_1 = "205A336B4E55"
+SER_BACK_LEFT_2 = "2061376C4243"
+SER_BACK_RIGHT_3 = "206D33614D43"
+SER_FRONT_RIGHT_4 = "206B336B4E55"
+# TODO: Use dictionary instead. cdccccbe and cdcccc3e would be mapped to keys of -40 and 40 respectively
+NEGATIVE_40 = binascii.a2b_hex("000000008400058208000000cdccccbe0000000032a34b88")
+POSITIVE_40 = binascii.a2b_hex("000000008400058208000000cdcccc3e000000000af69afb")
+
 def bulkWrite(dev, data, endpoint=0x02, timeout=None):
     dev.bulkWrite(endpoint, data, timeout=(1000 if timeout is None else timeout))
 
 def forward(serial, dev):
-    # TODO: Replace with one message and timestamp-packed last bytes, they shouldn't be much different
+    # TODO: Replace static file with one message and timestamp-packed last bytes, they shouldn't be much different
     comm=open("commtestcan").read().splitlines()
-    if serial == "206B336B4E55": # Front right, 4 DONE
-        bulkWrite(dev, binascii.a2b_hex("000000008400058208000000cdccccbe0000000032a34b88"))
-    elif serial == "206D33614D43": # Back right, 3
-        bulkWrite(dev, binascii.a2b_hex("000000008400058208000000cdccccbe0000000032a34b88"))
-    elif serial == "2061376C4243": #Back left, 2
-        bulkWrite(dev, binascii.a2b_hex("000000008400058208000000cdcccc3e000000000af69afb"))
-    elif serial == "205A336B4E55": # Front left, 1
-        bulkWrite(dev, binascii.a2b_hex("000000008400058208000000cdcccc3e000000000af69afb"))
+    if serial == SER_FRONT_LEFT_1: # Front left, 1
+        bulkWrite(dev, POSITIVE_40)
+    elif serial == SER_BACK_LEFT_2: # Back left, 2
+        bulkWrite(dev, POSITIVE_40)
+    elif serial == SER_BACK_RIGHT_3: # Back right, 3
+        bulkWrite(dev, NEGATIVE_40)
+    elif serial == SER_FRONT_RIGHT_4: # Front right, 4
+        bulkWrite(dev, NEGATIVE_40)
     else:
         raise Exception("Unknown serial detected: %s" % serial)
     while True:
