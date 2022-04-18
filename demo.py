da5794ec53ee03d5fad8e5744eb69178922772a8
Nathan Nye <noproto@users.noreply.github.com>
2022-04-18 18:12:08 -0400

    Improvements to locomotion

--- demo.py
+++ demo.py
@@ -21,11 +21,7 @@ SER_FRONT_RIGHT_4 = "206B336B4E55"
 
 # TODO: Better interface for motor controller protocol
 POSITIVE_HEX = "000000008400058208000000XXXXXXXX000000000af69afb"
-COMM_FORWARD = ["00000000802c058208000000100000000000000095e92111",
-                "00000000802c0582080000001000000000000000a6102211",
-                "00000000802c0582080000001000000000000000b6372211",
-                "00000000802c0582080000001000000000000000c75e2211",
-                "00000000802c0582080000001000000000000000d8852211"]
+COMM_FORWARD = "00000000802c058208000000100000000000000095e92111"
 
 def generate_speed(speed):
     return binascii.a2b_hex(POSITIVE_HEX.replace("XXXXXXXX", str(binascii.hexlify(struct.pack('<f', speed)), "ascii")))
@@ -35,7 +31,7 @@ CURRENT_ACTION = 0
 all_motors = []
 
 def move_forward(serial, dev):
-    global CURRENT_ACTION, FORWARD, MOTOR_SLEEP
+    global CURRENT_ACTION, FORWARD, MOTOR_SLEEP, COMM_FORWARD
     dev.claimInterface(0)
     # TODO: Replace static file with one message and timestamp-packed last bytes, they shouldn't be much different
     if serial == SER_FRONT_LEFT_1: # Front left, 1
@@ -49,15 +45,14 @@ def move_forward(serial, dev):
     else:
         raise Exception("Unknown serial detected: %s" % serial)
     try:
-        for line in COMM_FORWARD:
-            dev.bulkWrite(0x02, binascii.a2b_hex(line))
-            time.sleep(MOTOR_SLEEP)
+        dev.bulkWrite(0x02, binascii.a2b_hex(COMM_FORWARD))
+        time.sleep(MOTOR_SLEEP)
     except Exception as e:
         #print("Error: %s" % str(e))
         return
 
 def move_backward(serial, dev):
-    global CURRENT_ACTION, BACKWARD, MOTOR_SLEEP
+    global CURRENT_ACTION, BACKWARD, MOTOR_SLEEP, COMM_FORWARD
     dev.claimInterface(0)
     # TODO: Replace static file with one message and timestamp-packed last bytes, they shouldn't be much different
     if serial == SER_FRONT_LEFT_1: # Front left, 1
@@ -71,53 +66,50 @@ def move_backward(serial, dev):
     else:
         raise Exception("Unknown serial detected: %s" % serial)
     try:
-        for line in COMM_FORWARD:
-            dev.bulkWrite(0x02, binascii.a2b_hex(line))
-            time.sleep(MOTOR_SLEEP)
+        dev.bulkWrite(0x02, binascii.a2b_hex(COMM_FORWARD))
+        time.sleep(MOTOR_SLEEP)
     except Exception as e:
         #print("Error: %s" % str(e))
         return
 
 def spin_left(serial, dev):
-    global CURRENT_ACTION, LEFT, MOTOR_SLEEP
+    global CURRENT_ACTION, LEFT, MOTOR_SLEEP, COMM_FORWARD
     dev.claimInterface(0)
     # TODO: Replace static file with one message and timestamp-packed last bytes, they shouldn't be much different
     if serial == SER_FRONT_LEFT_1: # Front left, 1
-        dev.bulkWrite(0x02, generate_speed(CURRENT_SPEED), timeout=1000)
+        dev.bulkWrite(0x02, generate_speed(-CURRENT_SPEED), timeout=1000)
     elif serial == SER_BACK_LEFT_2: # Back left, 2
-        dev.bulkWrite(0x02, generate_speed(CURRENT_SPEED), timeout=1000)
+        dev.bulkWrite(0x02, generate_speed(-CURRENT_SPEED), timeout=1000)
     elif serial == SER_BACK_RIGHT_3: # Back right, 3
-        dev.bulkWrite(0x02, generate_speed(CURRENT_SPEED), timeout=1000)
+        dev.bulkWrite(0x02, generate_speed(-CURRENT_SPEED), timeout=1000)
     elif serial == SER_FRONT_RIGHT_4: # Front right, 4
-        dev.bulkWrite(0x02, generate_speed(CURRENT_SPEED), timeout=1000)
+        dev.bulkWrite(0x02, generate_speed(-CURRENT_SPEED), timeout=1000)
     else:
         raise Exception("Unknown serial detected: %s" % serial)
     try:
-        for line in COMM_FORWARD:
-            dev.bulkWrite(0x02, binascii.a2b_hex(line))
-            time.sleep(MOTOR_SLEEP)
+        dev.bulkWrite(0x02, binascii.a2b_hex(COMM_FORWARD))
+        time.sleep(MOTOR_SLEEP)
     except Exception as e:
         #print("Error: %s" % str(e))
         return
 
 def spin_right(serial, dev):
-    global CURRENT_ACTION, RIGHT, MOTOR_SLEEP
+    global CURRENT_ACTION, RIGHT, MOTOR_SLEEP, COMM_FORWARD
     dev.claimInterface(0)
     # TODO: Replace static file with one message and timestamp-packed last bytes, they shouldn't be much different
     if serial == SER_FRONT_LEFT_1: # Front left, 1
-        dev.bulkWrite(0x02, generate_speed(-CURRENT_SPEED), timeout=1000)
+        dev.bulkWrite(0x02, generate_speed(CURRENT_SPEED), timeout=1000)
     elif serial == SER_BACK_LEFT_2: # Back left, 2
-        dev.bulkWrite(0x02, generate_speed(-CURRENT_SPEED), timeout=1000)
+        dev.bulkWrite(0x02, generate_speed(CURRENT_SPEED), timeout=1000)
     elif serial == SER_BACK_RIGHT_3:  # Back right, 3
-        dev.bulkWrite(0x02, generate_speed(-CURRENT_SPEED), timeout=1000)
+        dev.bulkWrite(0x02, generate_speed(CURRENT_SPEED), timeout=1000)
     elif serial == SER_FRONT_RIGHT_4: # Front right, 4
-        dev.bulkWrite(0x02, generate_speed(-CURRENT_SPEED), timeout=1000)
+        dev.bulkWrite(0x02, generate_speed(CURRENT_SPEED), timeout=1000)
     else:
         raise Exception("Unknown serial detected: %s" % serial)
     try:
-        for line in COMM_FORWARD:
-            dev.bulkWrite(0x02, binascii.a2b_hex(line))
-            time.sleep(MOTOR_SLEEP)
+        dev.bulkWrite(0x02, binascii.a2b_hex(COMM_FORWARD))
+        time.sleep(MOTOR_SLEEP)
     except Exception as e:
         #print("Error: %s" % str(e))
         return
