f885428b71ea08d3fdb3b26bfa45b72809dbe292
celineramirez <80066301+celineramirez@users.noreply.github.com>
2022-05-11 15:25:32 -0400

    Update servo_controller.ino

--- servo_controller.ino
+++ servo_controller.ino
@@ -5,50 +5,45 @@ Servo myservo1;
 Servo myservo2;
 
 int readByte = 0; // initialize joule input
-
-int pos = 0;    // variable to store the servo position
+int move_cam = 10; // increment or decrement the position by 1
+int pos_x = 0; // x axis movement
+int pos_y = 0; // y axis movement
+int x = 0;
+int getVal = 0;
 
 void setup() {
-  Serial.begin(9600); // open serial port 9600 bps
-  myservo1.attach(9);  // attaches the servo on pin 9 to the servo object
-  myservo2.attach(3);  // attaches the servo on pin 3 to the servo object
-  
+  Serial.begin(115200); // open serial port 115200 bps
+  Serial.setTimeout(1);
+  myservo1.attach(9); // attaches the servo on pin 9 to the servo object
+  myservo2.attach(3); // attaches the servo on pin 3 to the servo object
+  readByte = Serial.read();
+  getVal = Serial.parseInt();
+
   // set up pins to take input from joule
   pinMode(9, INPUT);
   pinMode(3, INPUT);
 }
 
 void loop() {
-    int move_cam = 1; // increment or decrement the position by 1
-    int pos_x = 0; // x axis movement
-    int pos_y = 0; // y axis movement
+  Serial.println("Hello from arduino");
 
-// 1 is left, 2 is right
- if ((readByte = 1) || (readByte = 2)) {
-  for (pos_x = 90; pos_x <= 180; pos += 1) { // goes from 0 degrees to 180 degrees 
-    if (readByte = 1) {
-   pos_x = pos_x - move_cam ;
-    }
-    else if (readByte = 2) {
-   pos_x = pos_x + move_cam ;
-    }
-    // left right servo
+  if (Serial.available() > 0) {
+
+    if (getVal = 1) {
+      pos_x = pos_x - move_cam;
       myservo1.write(pos_x);
-   }
- }
- // 3 is down, 4 is up
-   else if((readByte = 3) || (readByte = 4)) {
-  for (pos_y = 90; pos_y <= 180; pos+= 1) {
-    if (readByte = 3){
-    pos_y = pos_y - move_cam;
-    }
-    else if (readByte = 4){
-    pos_y = pos_y + move_cam;
+    } else if (getVal = 2) {
+      pos_x = pos_x + move_cam;
+      myservo1.write(pos_x);
+    } else if (getVal = 3) {
+      pos_y = pos_y - move_cam;
+      myservo2.write(pos_y);
+    } else if (readByte = 4) {
+      pos_y = pos_y + move_cam;
+      myservo2.write(pos_y);
     }
   }
-  // up down servo
-    myservo2.write(pos_y);
-   }            
 
-  delay(10);                       // waits 10ms for the servo to reach the position
-  }
+
+  delay(10); // waits 10ms for the servo to reach the position
+}
