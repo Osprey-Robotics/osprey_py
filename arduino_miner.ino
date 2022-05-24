a8c9d964140bec1afaa15ab674d77e36c61a1a9b
Nathan Nye <noproto@users.noreply.github.com>
2022-05-24 06:07:25 -0400

    Update arduino_miner.ino

--- arduino_miner.ino
+++ arduino_miner.ino
@@ -1,59 +1,4 @@
 #include <Servo.h>
-#ifndef ARDPRINTF
-#define ARDPRINTF
-#define ARDBUFFER 16
-#include <stdarg.h>
-#include <Arduino.h>
-
-int ardprintf(char * str, ...) {
-    int i, count = 0, j = 0, flag = 0;
-    char temp[ARDBUFFER + 1];
-    for (i = 0; str[i] != '\0'; i++)
-        if (str[i] == '%') count++;
-
-    va_list argv;
-    va_start(argv, count);
-    for (i = 0, j = 0; str[i] != '\0'; i++) {
-        if (str[i] == '%') {
-            temp[j] = '\0';
-            Serial.print(temp);
-            j = 0;
-            temp[0] = '\0';
-
-            switch (str[++i]) {
-            case 'd':
-                Serial.print(va_arg(argv, int));
-                break;
-            case 'l':
-                Serial.print(va_arg(argv, long));
-                break;
-            case 'f':
-                Serial.print(va_arg(argv, double));
-                break;
-            case 'c':
-                Serial.print((char) va_arg(argv, int));
-                break;
-            case 's':
-                Serial.print(va_arg(argv, char * ));
-                break;
-            default:
-                ;
-            };
-        } else {
-            temp[j] = str[i];
-            j = (j + 1) % ARDBUFFER;
-            if (j == 0) {
-                temp[ARDBUFFER] = '\0';
-                Serial.print(temp);
-                temp[0] = '\0';
-            }
-        }
-    };
-    Serial.println();
-    return count + 1;
-}
-#undef ARDBUFFER
-#endif
 
 // Main constants
 Servo myservo1;
@@ -72,16 +17,27 @@ const int BUFFER_SIZE = 2;
 char buf[BUFFER_SIZE];
 int relay_1 = 7;
 int relay_2 = 6;
+int limit_bucket_ladder_top = 8;
+int limit_bucket_ladder_bottom = 10;
+int limit_deposition_forward = 11;
+int limit_actuator_extended = 12;
+int limit_deposition_back = 13;
 
 void setup() {
     Serial.begin(9600); // Open serial port 9600 bps
-    //Serial.setTimeout(1);
+    Serial.setTimeout(5);
     myservo1.attach(9); // Attaches the servo on pin 9 to the servo object
     myservo2.attach(3); // Attaches the servo on pin 3 to the servo object
 
     // Set up pins to take input from Joule
     pinMode(9, INPUT);
     pinMode(3, INPUT);
+
+    pinMode(limit_bucket_ladder_top, INPUT);
+    pinMode(limit_bucket_ladder_bottom, INPUT);
+    pinMode(limit_deposition_forward, INPUT);
+    pinMode(limit_actuator_extended, INPUT);
+    pinMode(limit_deposition_back, INPUT);
 
     pinMode(relay_1, OUTPUT);
     pinMode(relay_2, OUTPUT);
@@ -91,32 +47,52 @@ void setup() {
 }
 
 void loop() {
-    if (Serial.available() > 0) {
+    check_limit_switches();
+    if (Serial.available() >= 2) {
         // Read the incoming bytes
         int rlen = Serial.readBytes(buf, BUFFER_SIZE);
         if (rlen != BUFFER_SIZE) {
             return;
         }
+        Serial.println("ACK");
         memcpy(&(current_message.FunctionCode), buf, sizeof(byte));
         memcpy(&(current_message.Degrees), buf+1, sizeof(byte));
-        // Prints the received data
-        ardprintf("Received function %d with degrees %d", current_message.FunctionCode, current_message.Degrees);
         if (current_message.FunctionCode == 1) {
             myservo1.write(current_message.Degrees);
-            delay(10); // Waits 10ms for the servo to reach the position
+            delay(50); // Waits 50ms for the servo to reach the position
         } else if (current_message.FunctionCode == 2) {
             myservo2.write(current_message.Degrees);
-            delay(10); // Waits 10ms for the servo to reach the position
+            delay(50); // Waits 50ms for the servo to reach the position
         } else if (current_message.FunctionCode == 3) {
             // Actuator off
             digitalWrite(relay_1, LOW);
             digitalWrite(relay_2, LOW);
         } else if (current_message.FunctionCode == 4) {
-            // Actuator forward
-            digitalWrite(relay_1, HIGH);
-        } else if (current_message.FunctionCode == 5) {
             // Actuator reverse
             digitalWrite(relay_2, HIGH);
+        } else if (current_message.FunctionCode == 5 && digitalRead(limit_actuator_extended) == LOW) {
+            // Actuator forward
+            digitalWrite(relay_1, HIGH);
         }
+    }
+}
+
+void check_limit_switches() {
+    if (digitalRead(limit_bucket_ladder_top) == HIGH) {
+        Serial.println("Hit: limit_bucket_ladder_top");
+    }
+    if (digitalRead(limit_bucket_ladder_bottom) == HIGH) {
+        Serial.println("Hit: limit_bucket_ladder_bottom");
+    }
+    if (digitalRead(limit_deposition_forward) == LOW) {
+        Serial.println("Hit: limit_deposition_forward");
+    }
+    if (digitalRead(limit_actuator_extended) == HIGH) { // && digitalRead(relay_1) == HIGH) {
+        Serial.println("Hit: limit_actuator_extended");
+        digitalWrite(relay_1, LOW);
+        //digitalWrite(relay_2, LOW);
+    }
+    if (digitalRead(limit_deposition_back) == HIGH) {
+        Serial.println("Hit: limit_deposition_back");
     }
 }
