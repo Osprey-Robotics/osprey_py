52ec03593e6716ee69036cc291a9d8f6922d6aa5
William L. Thomson Jr. <wlt@o-sinc.com>
2024-02-03 21:26:11 -0500

    server.py: Added license header, retroactively all code is under Apache 2.0

--- server.py
+++ server.py
@@ -1,3 +1,9 @@
+#  Copyright 2022 Osprey Robotics - UNF
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#
+
 import serial as ser
 import socket
 import binascii
